027f09abdeb4f84196ae4cc25258683188ba9e27
twentyeightlabs <artur@twentyeightlabs.com>
2017-12-10 21:44:40 +0100

    load html template from directory

--- handler.py
+++ handler.py
@@ -1,7 +1,7 @@
 import json
 import requests
 import re
-import urllib
+import os
 
 
 def profit(event, context):
@@ -11,10 +11,11 @@ def profit(event, context):
     dane = json.loads(json_data)
     price = dane["data"]["amount"]
 
-    html = '''
-    <div> Current BTC price on GDAX is: <% price %> <div>'''
-    html_price = re.sub(r'<% price %>', price, html)
+    html_template = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/')
 
+    with open(os.path.join(html_template, 'index.html'), 'r') as template:
+        html = template.read()
+        html_price = re.sub(r'<% price %>', price, html)
 
     response = {
         "statusCode": 200,
